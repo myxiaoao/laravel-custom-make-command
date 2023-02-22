216d85ac72f048300b379f9bf4e078da341be829
Cooper <myxiaoao@gmail.com>
2023-02-22 10:01:07 +0800

    Update CommandServiceProvider.php

--- src/CommandServiceProvider.php
+++ src/CommandServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Cooper\CustomMake;
 
+use Composer\InstalledVersions;
 use Cooper\CustomMake\Commands\RepositoryMakeCommand;
 use Cooper\CustomMake\Commands\ServiceMakeCommand;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
 
 class CommandServiceProvider extends ServiceProvider
@@ -14,6 +16,26 @@ class CommandServiceProvider extends ServiceProvider
             $this->commands([
                 ServiceMakeCommand::class,
                 RepositoryMakeCommand::class
+            ]);
+        }
+
+        if (class_exists(AboutCommand::class) && class_exists(InstalledVersions::class)) {
+            AboutCommand::add('Custom Make Command', [
+                'Version' => InstalledVersions::getPrettyVersion('cooper/laravel-custom-make-command'),
+                'Created' => function (): string {
+                    $folders = [
+                        'Repositories',
+                        'Services'
+                    ];
+                    $createPaths = collect($folders)
+                        ->filter(fn (string $folder): bool => is_dir(app_path($folder)));
+
+                    if (! $createPaths->count()) {
+                        return '<fg=green;options=bold>NOT CREATED</>';
+                    }
+
+                    return "<fg=red;options=bold>CREATED:</> {$createPaths->join(', ')}";
+                },
             ]);
         }
     }
